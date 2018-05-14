0c06b68f8e65eb326d8bbb1083bc7b813f8d003a
Justin Tomlinson <justintomlinsonn@gmail.com>
2018-05-14 22:10:17 +0200

    Cleaned code
    Justin Tomlinson
    
    The program will now get rid of non-alphabetic characters and make everything
    lowercase for fair comparisons.

--- lyricFreq.py
+++ lyricFreq.py
@@ -5,11 +5,18 @@ from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 from sys import stdin
 from collections import Counter
+import re
+regex = re.compile('[^a-zA-Z]')
 
 NUMLYRICS = 10
 
 #Break up the words in the text file
 lyrics = [lyric for song in stdin for lyric in song.split()]
+
+#Get rid of non alphabetic characters and convert to lowercase
+for l in range(len(lyrics)):
+   lyrics[l] = lyrics[l].lower()
+   lyrics[l] = regex.sub('',lyrics[l])
 
 #Get the lyrics in order of frequency
 lyricsKeys = [Counter(lyrics).most_common(NUMLYRICS)[i][0]
